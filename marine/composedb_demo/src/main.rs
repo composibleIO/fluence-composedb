b39170e5e994baec2dcf38ff7a1d310ccb304dea
Joera <joeramulders@gmail.com>
2023-01-09 11:41:35 +0100

    improved readme

--- marine/composedb_demo/src/main.rs
+++ marine/composedb_demo/src/main.rs
@@ -8,6 +8,7 @@
 
 
 pub mod types;
+pub mod endpoints;
 pub mod curl;
 pub mod bin_requests;
 pub mod http_requests;
@@ -18,7 +19,6 @@ pub mod intermediaries;
 use std::fs;
 use std::env;
 use std::str;
-// use std::io;
 
 use marine_rs_sdk::marine;
 use marine_rs_sdk::module_manifest;
@@ -37,70 +37,14 @@ module_manifest!();
 pub fn main() {}
 
 #[marine]
-pub fn contractor_details(cid: &String) -> types::ContractorDetails {
-
-    let s = ipfs::dag_get(cid).unwrap();
-    
-    serde_json::from_str(&s).unwrap()
-}
-
-#[marine]
-pub fn has_intermediary(user_address: String, cid: String) -> Vec<TUIntermediary>  {
-
-    intermediaries::has_intermediary(user_address, format_intermediaries_endpoint(&cid))
-}
-
-#[marine]
-pub fn store_intermediary(intermediary: TUIntermediary, cid: String) -> Vec<TUIntermediary>  {
-
-    intermediaries::store_intermediary(intermediary, format_intermediaries_endpoint(&cid))
-}
-
-fn format_intermediaries_endpoint(cid: &String) -> String{
-
-    let details = contractor_details(cid);
-    let d = details.composedb.directions;
-
-    format!("http://{}{}_composedb:{}/graphql", d.namespace, d.n, details.composedb.indexes[1].port)
-}
-
-
-pub fn composedb_url(cid: &String) -> String {
-
-    let details = contractor_details(cid);
-    let d = details.composedb.directions;
-
-    format!("http://{}{}_composedb:{}", d.namespace, d.n, d.express_port)
-} 
-
-pub fn query_url(cid: &String, connection: Connection) -> String {
-
-    let details = contractor_details(cid);
-    let d = details.composedb.directions;
-
-    format!("http://{}{}_composedb:{}/graphql", d.namespace, d.n, connection.port)
-} 
-
-pub fn ceramic_url(cid: &String) -> String {
-
-    let details = contractor_details(cid);
-    let d = details.composedb.directions;
-
-    format!("http://{}{}_ceramic:{}", d.namespace, d.n, d.ceramic_port)
-} 
-
-
-#[marine]
 pub fn init(namespace: &String, n: &String, indexes: Vec<types::ComposeDbIndex>) -> String {
 
     // potentially do other stuff to create/augment config 
     let express_port : &str = "3000";
     let ceramic_port : &str = "7007";
   
-   
    // call composedb to get public info  & init readonly server
     let public_info = http_requests::init(namespace, n, &express_port.to_string());
-    // 
 
    // store on ipfs .. cid is included in fluence record 
     let directions = crate::types::ComposeDbDirections {
@@ -124,14 +68,14 @@ pub fn init(namespace: &String, n: &String, indexes: Vec<types::ComposeDbIndex>)
    let r = ipfs::dag_put(serde_json::to_value(obj).unwrap());
 
     r.unwrap()
-
 }
+
 
 #[marine] 
 pub fn deploy_index(index: ComposeDbIndex, contractor_cid: &String) -> String {
 
-    let composedb_url = composedb_url(contractor_cid);
-    let ceramic_url = ceramic_url(contractor_cid);
+    let composedb_url = endpoints::composedb_url(contractor_cid);
+    let ceramic_url = endpoints::ceramic_url(contractor_cid);
 
     let mut res1 = Result {
         stderr: "".to_owned(),
@@ -148,44 +92,47 @@ pub fn deploy_index(index: ComposeDbIndex, contractor_cid: &String) -> String {
     let res3 = bin_requests::compile(&index, &composedb_url, &ceramic_url);
 
     format!("{},{},{},{},{},{}", res1.stderr, res1.stdout, res2.stderr, res2.stdout, res3.stderr, res3.stdout)
-
 }
 
 #[marine] 
 pub fn serve(index: ComposeDbIndex, contractor_cid: &String) -> crate::types::Connection {
 
-    let composedb_url = composedb_url(contractor_cid);
-    
+    let composedb_url = endpoints::composedb_url(contractor_cid);
     http_requests::connect(index, "null".to_owned(), composedb_url)
 }
 
 #[marine] 
 pub fn connect(index: ComposeDbIndex, cap: String, contractor_cid: &String ) -> crate::types::Connection {
 
-    let composedb_url = composedb_url(contractor_cid);
-    
+    let composedb_url = endpoints::composedb_url(contractor_cid);
     http_requests::connect(index, cap, composedb_url)
 }
 
-// #[marine] 
-// pub fn storeKey(ethAddress: String, encryptedString: String, encryptedSymmetricKey: String, server_config: types::ComposeDbConfig) -> crate::types::Result {
+#[marine]
+pub fn contractor_details(cid: &String) -> types::ContractorDetails {
 
-//     let request = "{\"query\":\"mutation{createLitKeys(input:{content:{ethAddress:\\\"1xF816Bf1d588100b6cea06B12CCe53fA81E8124\\\",encryptedSymmetricKey:\\\"xxx\\\",encryptedString:\\\"fyu\\\"}}){document{ ethAddress,encryptedSymmetricKey,encryptedString}}}\"}";
+    let s = ipfs::dag_get(cid).unwrap();
+    serde_json::from_str(&s).unwrap()
+}
 
-//     requests::mutate(request.to_string(), KEY.to_string(), "5500".to_string(), server_config)
-// }
+#[marine]
+pub fn has_intermediary(user_address: String, cid: String) -> Vec<TUIntermediary>  {
+
+    intermediaries::has_intermediary(user_address, endpoints::format_intermediaries_endpoint(&cid))
+}
+
+#[marine]
+pub fn store_intermediary(intermediary: TUIntermediary, cid: String) -> Vec<TUIntermediary>  {
+
+    intermediaries::store_intermediary(intermediary, endpoints::format_intermediaries_endpoint(&cid))
+}
 
 #[marine] 
 pub fn mutate(display_name: String, cap: String, connection: Connection, contractor_cid: &String ) -> crate::types::Result {
 
-    let query_url = query_url(contractor_cid, connection);
-    // content object with : 
-    // slug for SimpleProfile
-    // slug for displayName
+    let query_url = endpoints::query_url(contractor_cid, connection);
     let request = "{\"query\":\"mutation{createSimpleProfile(input:{content:{displayName:\\\"$N\\\"}}){document{ displayName}}}\"}";
     let request = request.replace("$N", &display_name);
-
-    // validate capability ??? 
     
     http_requests::mutate(request.to_string(), query_url)
 }
@@ -193,10 +140,8 @@ pub fn mutate(display_name: String, cap: String, connection: Connection, contrac
 #[marine] 
 pub fn query(connection: Connection, contractor_cid: &String) -> types::Result {
 
-    let query_url = query_url(contractor_cid, connection);
-
+    let query_url = endpoints::query_url(contractor_cid, connection);
     let request = "{\"query\":\"query{simpleProfileIndex(first:100){edges{node{displayName,owner{id}}}}}\"}".to_owned();
-
     let response = http_requests::query(request, query_url);
 
     response
