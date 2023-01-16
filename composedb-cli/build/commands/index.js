3ad76fad6fac8a362e1bab5ce977ea18272db5ce
Joera <joeramulders@gmail.com>
2023-01-16 12:42:33 +0100

    yes

--- composedb-cli/build/commands/index.js
+++
@@ -1,6 +0,0 @@
-import { ComposeClient } from '@composedb/client';
-import { definition } from '../definitions/tu_profile--definition.js';
-export const resources = (ceramic) => {
-    const client = new ComposeClient({ ceramic, definition });
-    return client.resources;
-};
--- composedb-cli/build/services/ceramic.service.js
+++
@@ -1,8 +0,0 @@
-import { CeramicClient } from '@ceramicnetwork/http-client';
-import { writeEncodedCompositeRuntime } from '@composedb/devtools-node';
-export const ceramic = (ceramic) => {
-    return new CeramicClient(ceramic);
-};
-export const compile = async (ceramic, name) => {
-    return await writeEncodedCompositeRuntime(ceramic, '../composites/' + name + '.json', './' + name + '--runtime.js');
-};
